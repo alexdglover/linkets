e50d4ff5479e429ff847e91ff1a1b455343610a5
Shaun Payne <shaun.payne@vouch.us>
2021-12-13 18:23:43 -0800

    initial update; css styling, included status

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
-# Linkets
+### Dependencies
 
-App/widget for linking Zendesk tickets. Tickets can be linked together with specific relationships,
-like "Blocks", "Is Parent", or simply "Relates to." All linking is achieved by using tags and
-client side JavaScript, so your data stays safely within Zendesk. The number of links is only
-limited by the tag field constraint of 5096 characters, otherwise there are no limits.
+- NodeJS
+  - Install `node` via nodejs.org or `nvm`
+- ZCLI
+  - Install Zendesk CLI: `npm install @zendesk/zcli -g`
 
-Built with ZAFv2.
+### Setup
 
-"Linkets" is a portmanteau of "linked tickets."
+- Set up your credentials to Zendesk: `zcli login -i`
 
-## Issues
+### Run
 
-Please submit bug reports/issues on the [GitHub repo](https://github.com/alexdglover/linkets/issues). Pull requests are welcome.
+- Start the server: `zcli apps:server`
+- Open zendesk sandbox and add `?zcli_apps=true` to the end of your Zendesk URL
 
-### Screenshot(s):
-![image](https://user-images.githubusercontent.com/25018339/31585812-a62887c2-b18d-11e7-87eb-76c8e412d0f1.png)
+### Deploying to Sandbox
 
-![image](https://user-images.githubusercontent.com/25018339/31588364-48ce68c0-b1b6-11e7-8328-7d4025ff6332.png)
+- Increment the build version: `zcli apps:bump [option]`
+  - Options:
+    - -M, --major Increments the major version by 1
+    - -m, --minor Increments the minor version by 1
+    - -p, --patch Increments the patch version by 1
+- Clean the app: `zcli apps:clean`
+- Validate the app: `zcli apps:validate`
+- Push the update: `zcli apps:update`
 
-![image](https://user-images.githubusercontent.com/25018339/31588370-678156a6-b1b6-11e7-87ce-1449ef600d09.png)
+### Deploying to Production
 
-![image](https://user-images.githubusercontent.com/25018339/31588375-8945a026-b1b6-11e7-9bcf-b757274d7810.png)
+- (If you haven't incremented yet) Increment the build version: `zcli apps:bump [option]`
+  - Options:
+    - -M, --major Increments the major version by 1
+    - -m, --minor Increments the minor version by 1
+    - -p, --patch Increments the patch version by 1
+- Clean the app: `zcli apps:clean`
+- Validate the app: `zcli apps:validate`
+- Package the update: `zcli apps:package`
+- Navigate to the [Zendesk Apps Management Page](https://vouchinc.zendesk.com/agent/admin/apps/manage)
+- Find the 'Linkets' app, click settings and update.
+- Choose the file and upload.
--- assets/main.js
+++ assets/main.js
@@ -1,374 +1,250 @@
-var debug = false;
-
-var origin = '';
-
-$(function() {
-  var client = ZAFClient.init();
-  client.invoke('resize', { width: '100%', height: '400px' });
-  client.context().then(
-    function(data){
-      origin = 'https://' + data['account']['subdomain'] + '.zendesk.com';
-      if (debug) {
-        console.log(data);
-        console.log(origin);
-      }
-    }
-  )
+$(() => {
+  const client = ZAFClient.init();
+  client.invoke("resize", { width: "320px", height: "400px" });
+  client.context().then((data) => {
+    const origin = `https://${data["account"]["subdomain"]}.zendesk.com`;
+  });
   processLinkedTickets();
-
-  $('.get_help').on('click', showDeflectionModal);
-  function showDeflectionModal(){
-    client.invoke('instances.create', {
-      location: 'modal',
-      url: 'assets/deflection-modal.html'
-    }).then(function(modalContext) {
-      // The modal is on the screen, now resize it
-      var modalClient = client.instance(modalContext['instances.create'][0].instanceGuid);
-      modalClient.invoke('resize', { width: '40vw', height: '100%' });
-    });
-  }
-
 });
 
-function processLinkedTickets() {
+processLinkedTickets = () => {
   // Clear any populated divs first
-  $("#candidate-tickets").html('')
-  $("#content").html('')
+  $("#candidate-tickets").html("");
+  $("#content").html("");
 
-  var client = ZAFClient.init();
-  client.get('ticket.tags').then(
-    function(data) {
-      var tags = data['ticket.tags'];
-      var linkedTickets = parseLinkedTickets(tags);
-      if (linkedTickets.length == 0) {
-        showNoLinkedTickets();
-      }
-      else {
-        getLinkedTicketDetail(client, linkedTickets);
-      }
-    }
-  );
-}
+  const client = ZAFClient.init();
 
-function parseLinkedTickets(tags) {
-  var linkedTickets = [];
-  for (i=0; i < tags.length; i++) {
-    if (tags[i].startsWith('lnkt:')) {
-      linkedTickets.push(tags[i]);
-    }
-  }
-  if (debug) {
-    console.log('Linked tickets:')
-    console.log(linkedTickets);
-  }
+  client.get("ticket.tags").then((data) => {
+    const tags = data["ticket.tags"];
+    const linkedTickets = parseLinkedTickets(tags);
+
+    if (linkedTickets.length == 0) showNoLinkedTickets();
+    else getLinkedTicketDetail(client, linkedTickets);
+  });
+};
+
+parseLinkedTickets = (tags) => {
+  let linkedTickets = [];
+
+  tags.map((tag) => {
+    if (tag.startsWith("lnkt:")) linkedTickets.push(tag);
+  });
+
   return linkedTickets;
-}
+};
 
-function getLinkedTicketDetail(client, linkedTickets) {
-  var raw_ids = [];
-  for (i=0; i < linkedTickets.length; i++) {
-    raw_ids.push(linkedTickets[i].split(':')[2]);
-  }
-  raw_ids = raw_ids.join(',')
-  if (debug) {
-    console.log('Raw tickets IDs are:');
-    console.log(raw_ids);
-  }
-  var settings = {
-    url: '/api/v2/tickets/show_many.json?ids=' + raw_ids,
-    type: 'GET',
-    dataType: 'json',
+getLinkedTicketDetail = (client, linkedTickets) => {
+  let raw_ids = [];
+
+  linkedTickets.map((ticket) => {
+    raw_ids.push(ticket.split(":")[2]);
+  });
+
+  raw_ids = raw_ids.join(",");
+  const settings = {
+    url: "/api/v2/tickets/show_many.json?ids=" + raw_ids,
+    type: "GET",
+    dataType: "json",
   };
 
   client.request(settings).then(
-    function(data) {
-      if (debug) {
-        console.log('Raw API return value:');
-        console.log(data);
-        console.log('Calling showLinkedTickets');
-      }
-      showLinkedTickets(linkedTickets, data['tickets']);
-    },
-    function(response) {
-      showApiError(response);
-    }
+    (data) => showLinkedTickets(linkedTickets, data["tickets"]),
+    (error) => showApiError(error)
   );
-}
+};
 
-function showLinkedTickets(linkedTickets, data) {
-  var display_data = {};
-  for (i=0; i < linkedTickets.length; i++){
-    if (debug) {
-      console.log('Current linkedTicket object:');
-      console.log(linkedTickets[i]);
-    }
-    var id = linkedTickets[i].split(':')[2]
-    var relationship = linkedTickets[i].split(':')[1]
-    var ticket_data = {}
-    for (x=0; x < data.length; x++) {
-      if (data[x]['id'] == id) {
-        if (debug) {
-          console.log('Found matching ticket in data');
-          console.log(data[x]);
-        }
-        ticket_data = data[x]
-      }
-    }
-    display_data[i] = {
-      'id': id,
-      'relationship': translateRelationship(relationship),
-      'relationshipCode': relationship,
-      'subject': ticket_data['subject']
-    }
-  }
+showLinkedTickets = (linkedTickets, data) => {
+  let display_data = {};
 
-  if (debug) { console.log(display_data); }
-  var display_info = {
-    'ticket': display_data
-  }
-  var source = $("#linked-ticket-template").html();
-  var template = Handlebars.compile(source);
-  var html = template(display_info);
-  $("#content").html(html);
-}
+  linkedTickets.map((ticket, index) => {
+    const id = ticket.split(":")[2];
+    const relationship = ticket.split(":")[1];
+    let ticket_data = {};
 
-function showApiError(response) {
-  var error_data = {
-    'status': response.status,
-    'statusText': response.statusText
+    data.map((record) => {
+      if (record["id"] == id) ticket_data = record;
+    });
+
+    display_data[index] = {
+      id: id,
+      relationship: translateRelationship(relationship),
+      relationshipCode: relationship,
+      subject: ticket_data["subject"],
+      status: ticket_data["status"],
+    };
+  });
+
+  const display_info = {
+    ticket: display_data,
   };
-  var source = $("#error-template").html();
-  var template = Handlebars.compile(source);
-  var html = template(error_data);
+  const source = $("#linked-ticket-template").html();
+  const template = Handlebars.compile(source);
+  const html = template(display_info);
+
   $("#content").html(html);
-}
+};
 
-function showNoLinkedTickets() {
-  var source = $("#no-ticket-template").html();
-  var template = Handlebars.compile(source);
-  var html = template();
+showApiError = (response) => {
+  const error_data = {
+    status: response.status,
+    statusText: response.statusText,
+  };
+  const source = $("#error-template").html();
+  const template = Handlebars.compile(source);
+  const html = template(error_data);
   $("#content").html(html);
-}
+};
 
-function getSearchResults() {
-  $('#candidate-ticket-loading-icon').show()
+showNoLinkedTickets = () => {
+  const source = $("#no-ticket-template").html();
+  const template = Handlebars.compile(source);
+  const html = template();
+  $("#content").html(html);
+};
 
-  var client = ZAFClient.init();
-  var query = $("#linked-ticket-search").val();
-  var settings = {
-    url: '/api/v2/search.json?query=type:ticket ' + query,
-    type: 'GET',
-    dataType: 'json',
+getSearchResults = () => {
+  $("#candidate-ticket-loading-icon").show();
+
+  const client = ZAFClient.init();
+  const query = $("#linked-ticket-search").val();
+  const settings = {
+    url: "/api/v2/search.json?query=type:ticket " + query,
+    type: "GET",
+    dataType: "json",
   };
 
   client.request(settings).then(
-    function(data) {
-      if (debug) {
-        console.log('Raw API return value:');
-        console.log(data);
-        console.log('Calling showSearchResults');
-      }
-      $('#candidate-ticket-loading-icon').hide();
-      showSearchResults(data['results']);
+    (data) => {
+      $("#candidate-ticket-loading-icon").hide();
+      showSearchResults(data["results"]);
     },
-    function(response) {
-      $('#candidate-ticket-loading-icon').hide();
-      showApiError(response);
+    (error) => {
+      $("#candidate-ticket-loading-icon").hide();
+      showApiError(error);
     }
   );
-}
+};
 
-function showSearchResults(data) {
-  var display_data = {};
-  if (debug) { console.log(data); }
-  for (i=0; i < data.length; i++){
-    display_data[data[i]['id']] = {
-      'id': data[i]['id'],
-      'subject': data[i]['subject']
-    }
-  }
+showSearchResults = (data) => {
+  let display_data = {};
 
-  if (debug) { console.log(display_data); }
-  var display_info = {
-    'ticket': display_data
-  }
-  var source = $("#candidate-ticket-template").html();
-  var template = Handlebars.compile(source);
-  var html = template(display_info);
+  data.map((item) => {
+    display_data[item["id"]] = {
+      id: item["id"],
+      subject: item["subject"],
+      status: item["status"],
+    };
+  });
+
+  const display_info = {
+    ticket: display_data,
+  };
+  const source = $("#candidate-ticket-template").html();
+  const template = Handlebars.compile(source);
+  const html = template(display_info);
   $("#candidate-tickets").html(html);
-}
+};
 
-function linkTicket(id, relationship) {
-  var client = ZAFClient.init();
-  client.get('ticket.id').then(function(data) {
-    var this_ticket_id = data['ticket.id'];
-    if (debug) {
-      console.log('External ID is ' + id + ' and inverted relationship is ' + invertRelationship(relationship));
-      console.log('This ticket ID is ' + this_ticket_id + ' and relationship is ' + relationship);
-    }
-    var externalTicketTagText = 'lnkt:' + invertRelationship(relationship) + ':' + this_ticket_id;
-    var thisTicketTagText = 'lnkt:' + relationship + ':' + id;
-    if (debug) {
-      console.log(externalTicketTagText);
-      console.log(thisTicketTagText);
-    }
+linkTicket = (id, relationship) => {
+  const client = ZAFClient.init();
+  client.get("ticket.id").then((data) => {
+    const this_ticket_id = data["ticket.id"];
+    const externalTicketTagText = `lnkt:${invertRelationship(
+      relationship
+    )}:${this_ticket_id}`;
+    const thisTicketTagText = `lnkt:${relationship}:${id}`;
 
-    var errors = 0;
-
-    client.request({
-      url: '/api/v2/tickets/' + id + '/tags.json',
-      type: 'PUT',
-      contentType: 'application/json',
+    const pairedTicketUpdate = client.request({
+      url: "/api/v2/tickets/" + id + "/tags.json",
+      type: "PUT",
+      contentType: "application/json",
       data: JSON.stringify({
-        "tags": [externalTicketTagText]
-      })
-    }).then(
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-      },
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-        errors++;
-      }
-    )
+        tags: [externalTicketTagText],
+      }),
+    });
 
-    client.request({
-      url: '/api/v2/tickets/' + this_ticket_id + '/tags.json',
-      type: 'PUT',
-      contentType: 'application/json',
+    const selectedTicketUpdate = client.request({
+      url: "/api/v2/tickets/" + this_ticket_id + "/tags.json",
+      type: "PUT",
+      contentType: "application/json",
       data: JSON.stringify({
-        "tags": [thisTicketTagText]
+        tags: [thisTicketTagText],
+      }),
+    });
+
+    Promise.all([pairedTicketUpdate, selectedTicketUpdate])
+      .then((values) => {
+        $("#candidate-tickets").html("<h2>Linked successfully!</h2>");
+        setTimeout(processLinkedTickets, 3500);
       })
-    }).then(
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-      },
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-        errors++;
-      }
-    )
+      .catch((error) => {
+        $("#candidate-tickets").html("<h2>Error when linking tickets!</h2>");
+      });
+  });
+};
 
-    if(errors == 0){
-      $("#candidate-tickets").html('<h2>Linked successfully!</h2>');
-      setTimeout(processLinkedTickets, 1500);
-    }
-    else {
-      $("#candidate-tickets").html('<h2>Error when linking tickets!</h2>');
-    }
-  })
-}
+unlinkTicket = (id, relationship) => {
+  const client = ZAFClient.init();
+  client.get("ticket.id").then(function (data) {
+    const this_ticket_id = data["ticket.id"];
+    const externalTicketTagText =
+      "lnkt:" + invertRelationship(relationship) + ":" + this_ticket_id;
+    const thisTicketTagText = "lnkt:" + relationship + ":" + id;
 
-function unlinkTicket(id, relationship) {
-  var client = ZAFClient.init();
-  client.get('ticket.id').then(function(data) {
-    var this_ticket_id = data['ticket.id'];
-    if (debug) {
-      console.log('External ID is ' + id + ' and inverted relationship is ' + invertRelationship(relationship));
-      console.log('This ticket ID is ' + this_ticket_id + ' and relationship is ' + relationship);
-    }
-    var externalTicketTagText = 'lnkt:' + invertRelationship(relationship) + ':' + this_ticket_id;
-    var thisTicketTagText = 'lnkt:' + relationship + ':' + id;
-    if (debug) {
-      console.log(externalTicketTagText);
-      console.log(thisTicketTagText);
-    }
-
-    var errors = 0;
-
-    client.request({
-      url: '/api/v2/tickets/' + id + '/tags.json',
-      type: 'DELETE',
-      contentType: 'application/json',
+    const pairedTicketUpdate = client.request({
+      url: "/api/v2/tickets/" + id + "/tags.json",
+      type: "DELETE",
+      contentType: "application/json",
       data: JSON.stringify({
-        "tags": [externalTicketTagText]
-      })
-    }).then(
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-      },
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-        errors++;
-      }
-    )
+        tags: [externalTicketTagText],
+      }),
+    });
 
-    client.request({
-      url: '/api/v2/tickets/' + this_ticket_id + '/tags.json',
-      type: 'DELETE',
-      contentType: 'application/json',
+    const selectedTicketUpdate = client.request({
+      url: "/api/v2/tickets/" + this_ticket_id + "/tags.json",
+      type: "DELETE",
+      contentType: "application/json",
       data: JSON.stringify({
-        "tags": [thisTicketTagText]
+        tags: [thisTicketTagText],
+      }),
+    });
+
+    Promise.all([pairedTicketUpdate, selectedTicketUpdate])
+      .then((values) => {
+        $("#candidate-tickets").html("<h2>Unlinked successfully!</h2>");
+        setTimeout(processLinkedTickets, 3500);
       })
-    }).then(
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-      },
-      function(data, response) {
-        if (debug) {
-          console.log(data);
-          console.log(response);
-        }
-        errors++;
-      }
-    )
+      .catch((error) => {
+        $("#candidate-tickets").html("<h2>Error when unlinking tickets!</h2>");
+      });
+  });
+};
 
-    if(errors == 0){
-      $("#candidate-tickets").html('<h2>Unlinked successfully!</h2>');
-      setTimeout(processLinkedTickets, 1500);
-    }
-    else {
-      $("#candidate-tickets").html('<h2>Error when unlinking tickets!</h2>');
-    }
-  })
-}
+invertRelationship = (relationshipCode) => {
+  const invertedRelationships = {
+    blckdby: "blcks",
+    blcks: "blckdby",
+    prnt: "chld",
+    chld: "prnt",
+    rlts: "rlts",
+  };
 
-function invertRelationship(relationshipCode) {
-  var invertedRelationships = {
-    'blckdby': 'blcks',
-    'blcks': 'blckdby',
-    'prnt': 'chld',
-    'chld': 'prnt',
-    'rlts': 'rlts'
-  }
+  return invertedRelationships[relationshipCode];
+};
 
-  return invertedRelationships[relationshipCode]
-}
+translateRelationship = (relationshipCode) => {
+  const relationships = {
+    blckdby: "Blocked By",
+    blcks: "Blocks",
+    prnt: "Parent",
+    chld: "Child",
+    rlts: "Relates To",
+  };
 
-function translateRelationship(relationshipCode) {
-  var relationships = {
-    'blckdby': 'Blocked By',
-    'blcks': 'Blocks',
-    'prnt': 'Parent',
-    'chld': 'Child',
-    'rlts': 'Relates To'
-  }
+  return relationships[relationshipCode];
+};
 
-  return relationships[relationshipCode]
-}
-
-function openTicket(id){
-  var client = ZAFClient.init();
-  client.invoke('routeTo', 'ticket', id);
-}
+openTicket = (id) => {
+  const client = ZAFClient.init();
+  client.invoke("routeTo", "ticket", id);
+};
